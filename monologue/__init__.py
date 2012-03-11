8d2c29edcf150a18dbf5b1ef3410d095c746e8ef
Feth AREZKI <feth@tuttu.info>
2012-03-11 21:21:46 +0100

    [python3.2 compat] use 'from . import blah'

--- monologue/__init__.py
+++ monologue/__init__.py
@@ -9,6 +9,6 @@ For convenience, all standard debug level are importable from this module
 from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 from .core import get_logger, PROGRESS
-import core
+from . import core
 
 get_logger = get_logger
